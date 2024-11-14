fb9978c0d99c58c2a9ca3fafc4f4925118264d3a
ayesha720 <51579690+ayesha720@users.noreply.github.com>
2024-11-14 14:20:26 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <>
       <div className='app'>
      <Navbar/>
-        <Home/>
+      
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/cart' element={<Cart/>}/>
